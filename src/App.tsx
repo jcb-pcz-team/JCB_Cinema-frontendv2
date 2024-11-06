4b4d132a193bc113b2cece4a78cd5642f3d29a22
Kamil Czudaj <kamilcz2001@gmail.com>
2024-11-06 19:22:05 +0100

    feat: add MailLayout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import {Header} from "./components/Header/Header.tsx";
-import {Footer} from "./components/Footer/Footer.tsx";
-
 export const App = () => {
   return (
     <>
-      <Header/>
-      <Footer/>
     </>
   )
 };
---
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -0,0 +1,18 @@
+import './MainLayout.scss'
+import React from 'react';
+import { Header } from '../../components/Header/Header';
+import { Footer } from '../../components/Footer/Footer';
+
+interface MainLayoutProps {
+    children: React.ReactNode;
+}
+
+export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+    return (
+        <div className="main-layout">
+            <Header />
+            <main>{children}</main>
+            <Footer />
+        </div>
+    );
+};
