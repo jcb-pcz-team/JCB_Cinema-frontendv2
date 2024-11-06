8d02e42f94dada4744934a6c439671d24cf6c5b4
Kamil Czudaj <kamilcz2001@gmail.com>
2024-11-06 19:04:14 +0100

    feat: add Footer component with social media icons
    style: add global overlay class for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import {Header} from "./components/Header/Header.tsx";
+import {Footer} from "./components/Footer/Footer.tsx";
 
 export const App = () => {
   return (
-    <Header/>
+    <>
+      <Header/>
+      <Footer/>
+    </>
   )
 };
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,26 @@
+import './Footer.scss';
+
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <section className="footer__info overlay">
+                <img src="src/assets/images/logo.svg" alt="jcb cinema" className="footer__logo logo"/>
+                <p className="footer__description">
+                    JCB CINEMA, where the magic of film meets modern comfort. Enjoy the latest blockbusters and timeless
+                    classics in our state-of-the-art theater with immersive sound and crystal clarity!
+                </p>
+                <div className="footer__social">
+                    <span className="footer__social-text">JOIN US ON</span>
+                    <div className="footer__social-icons">
+                        <img src="src/assets/images/instagram.svg" alt="Instagram" className="footer__social-icon"/>
+                        <img src="src/assets/images/x.svg" alt="X" className="footer__social-icon"/>
+                        <img src="src/assets/images/facebook.svg" alt="Facebook" className="footer__social-icon"/>
+                    </div>
+                </div>
+            </section>
+            <p className="footer__copyright">
+                All rights reserved JCB CINEMA 2024 &copy;
+            </p>
+        </footer>
+    );
+};
