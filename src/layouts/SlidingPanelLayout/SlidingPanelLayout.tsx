a0f9e31b275adefa0bfbd006cb933c8225d7e67f
Kamil Czudaj <kamilcz2001@gmail.com>
2024-11-11 19:48:47 +0100

    feat: add reusable sliding panel layout component for mobile views

---
+++ src/layouts/SlidingPanelLayout/SlidingPanelLayout.tsx
@@ -0,0 +1,25 @@
+import './SlidingPanelLayout.scss';
+import React, { ReactNode } from "react";
+
+interface SlidingPanelLayoutProps {
+    title: string;
+    toggle: () => void;
+    isOpen: boolean;
+    children: ReactNode;
+}
+
+export const SlidingPanelLayout: React.FC<SlidingPanelLayoutProps> = ({ title, toggle, isOpen, children }) => {
+    return (
+        <div className={`sliding-panel-layout ${isOpen ? 'sliding-panel-layout--open' : ''}`}>
+            <div className="sliding-panel-layout__header">
+                <p className="sliding-panel-layout__title">{title}</p>
+                <button onClick={() => toggle()} className="sliding-panel-layout__button-close">
+                    <img src="src/assets/images/icon-cross.svg" alt="cross"/>
+                </button>
+            </div>
+            <div className="sliding-panel-layout__content">
+                {children}
+            </div>
+        </div>
+    );
+};
