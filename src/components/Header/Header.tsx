03ebfb9ae53d95a5272da5058a83b9cc0f038f00
Kamil Czudaj <kamilcz2001@gmail.com>
2024-11-11 21:32:04 +0100

    feat: add search panel component for mobile views

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,10 +2,12 @@ import './Header.scss';
 import React, {useState} from "react";
 import {MenuSlideLayout} from "../MenuSlideLayout/MenuSlideLayout.tsx";
 import {LoginSlideLayout} from "../LoginSlideLayout/LoginSlideLayout.tsx";
+import {SearchSlideLayout} from "../SearchSlideLayout/SearchSlideLayout.tsx";
 
 export const Header : React.FC = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [isLoginOpen, setIsLoginOpen] = useState(false);
+    const [isSearchOpen, setIsSearchOpen] = useState(false);
 
     function toggleMenu() {
         setIsMenuOpen(!isMenuOpen);
@@ -15,10 +17,19 @@ export const Header : React.FC = () => {
         setIsLoginOpen(!isLoginOpen);
     }
 
+    function toggleSearch() {
+        setIsSearchOpen(!isSearchOpen);
+    }
+
     return (
         <header className="header">
             <img src="src/assets/images/logo.svg" alt="jcb cinema" className="logo"/>
             <nav className='header__nav'>
+                <button onClick={toggleSearch}
+                        className="button-search"
+                >
+                    <img src="src/assets/images/icon-search.svg" alt="login"/>
+                </button>
                 <button onClick={toggleLogin}
                         className="button-login"
                 >
@@ -32,6 +43,7 @@ export const Header : React.FC = () => {
             </nav>
             <MenuSlideLayout isOpen={isMenuOpen} toggleMenu={toggleMenu}/>
             <LoginSlideLayout isOpen={isLoginOpen} toggleLogin={toggleLogin}/>
+            <SearchSlideLayout isOpen={isSearchOpen} toggleSearch={toggleSearch}/>
         </header>
     );
 };
