947078260c601c9b458833d82977d132c5defd6f
Kamil <kamilcz2001@gmail.com>
2024-11-17 23:22:13 +0100

    styles: enhance HeaderMobile and SlidingPanelLayout visuals

--- src/components/HeaderMobile/HeaderMobile.tsx
+++ src/components/HeaderMobile/HeaderMobile.tsx
@@ -1,49 +1,66 @@
 import './HeaderMobile.scss';
-import React, {useState} from "react";
-import {MenuSlideLayout} from "../MenuSlideLayout/MenuSlideLayout.tsx";
-import {LoginSlideLayout} from "../LoginSlideLayout/LoginSlideLayout.tsx";
-import {SearchSlideLayout} from "../SearchSlideLayout/SearchSlideLayout.tsx";
+import React, { useReducer } from "react";
+import { MenuSlideLayout } from "../MenuSlideLayout/MenuSlideLayout.tsx";
+import { LoginSlideLayout } from "../LoginSlideLayout/LoginSlideLayout.tsx";
+import { SearchSlideLayout } from "../SearchSlideLayout/SearchSlideLayout.tsx";
 
-export const HeaderMobile : React.FC = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [isLoginOpen, setIsLoginOpen] = useState(false);
-    const [isSearchOpen, setIsSearchOpen] = useState(false);
+type Action =
+    | { type: 'toggle_menu' }
+    | { type: 'toggle_login' }
+    | { type: 'toggle_search' };
 
-    function toggleMenu() {
-        setIsMenuOpen(!isMenuOpen);
+type State = {
+    isMenuOpen: boolean;
+    isLoginOpen: boolean;
+    isSearchOpen: boolean;
+};
+
+function reducer(state: State, action: Action): State {
+    switch (action.type) {
+        case 'toggle_menu':
+            return { ...state, isMenuOpen: !state.isMenuOpen };
+        case 'toggle_login':
+            return { ...state, isLoginOpen: !state.isLoginOpen };
+        case 'toggle_search':
+            return { ...state, isSearchOpen: !state.isSearchOpen };
+        default:
+            return state;
     }
+}
 
-    function toggleLogin() {
-        setIsLoginOpen(!isLoginOpen);
-    }
-
-    function toggleSearch() {
-        setIsSearchOpen(!isSearchOpen);
-    }
+export const HeaderMobile: React.FC = () => {
+    const [state, dispatch] = useReducer(reducer, {
+        isMenuOpen: false,
+        isLoginOpen: false,
+        isSearchOpen: false,
+    });
 
     return (
         <header className="header-mobile">
-            <img src="src/assets/images/logo.svg" alt="jcb cinema" className="logo"/>
-            <nav className='header-mobile__navbar'>
-                <button onClick={toggleSearch}
-                        className="button-search"
+            <img src="src/assets/images/logo.svg" alt="jcb cinema" className="logo" />
+            <nav className="header-mobile__navbar">
+                <button
+                    onClick={() => dispatch({ type: 'toggle_search' })}
+                    className="button-search"
                 >
-                    <img src="src/assets/images/icon-search.svg" alt="login"/>
+                    <img src="src/assets/images/icon-search.svg" alt="search" />
                 </button>
-                <button onClick={toggleLogin}
-                        className="button-login"
+                <button
+                    onClick={() => dispatch({ type: 'toggle_login' })}
+                    className="button-login"
                 >
-                    <img src="src/assets/images/icon-profile.svg" alt="login"/>
+                    <img src="src/assets/images/icon-profile.svg" alt="login" />
                 </button>
-                <button onClick={toggleMenu}
-                        className="button-menu"
+                <button
+                    onClick={() => dispatch({ type: 'toggle_menu' })}
+                    className="button-menu"
                 >
-                    <img src="src/assets/images/icon-menu_hamburger.svg" alt="menu_hamburger"/>
+                    <img src="src/assets/images/icon-menu_hamburger.svg" alt="menu_hamburger" />
                 </button>
             </nav>
-            <MenuSlideLayout isOpen={isMenuOpen} toggleMenu={toggleMenu}/>
-            <LoginSlideLayout isOpen={isLoginOpen} toggleLogin={toggleLogin}/>
-            <SearchSlideLayout isOpen={isSearchOpen} toggleSearch={toggleSearch}/>
+            <MenuSlideLayout isOpen={state.isMenuOpen} toggleMenu={() => dispatch({ type: 'toggle_menu' })} />
+            <LoginSlideLayout isOpen={state.isLoginOpen} toggleLogin={() => dispatch({ type: 'toggle_login' })} />
+            <SearchSlideLayout isOpen={state.isSearchOpen} toggleSearch={() => dispatch({ type: 'toggle_search' })} />
         </header>
     );
 };
